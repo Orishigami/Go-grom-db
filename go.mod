a8967502064a508a85d99402f99e5f14b3cfadca
Orishigami <641463017@crru.ac.th>
2024-01-18 15:25:44 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/orishigami/go-grom-db
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,67 @@
+// main.go
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/anusornc/go-gorm-db/db"
+	"github.com/anusornc/go-gorm-db/models"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	dbType := os.Getenv("DB_TYPE")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+
+	//เชื่อมฐานข้อมูล
+	database, err := db.ConnectDatabase(dbType, dbUser, dbPassword, dbHost, dbPort, dbName)
+	if err != nil {
+		log.Fatalf("failed to connect database: %v", err)
+	}
+
+	//สร้างตารางในฐานข้อมูล
+	err = database.AutoMigrate(&models.Item{})
+	if err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
+
+	//สร้างตัวแปร itemRepo เพื่อเรียกใช้งานฟังก์ชั้น
+	itemRepo := models.NewItemRepository(database)
+
+	r := gin.Default()
+
+	// api /items จะเป็นการเรียกใช้งานฟังก์ชัน GetItems ใน ItemRepository
+	r.GET("/items", itemRepo.GetItems)
+
+	// api /items/:id จะเป็นการเรียกใช้งานฟังก์ชัน GetItem ใน ItemRepository
+	r.POST("/items", itemRepo.PostItem)
+
+	// api /items/:id จะเป็นการเรียกใช้งานฟังก์ชัน GetItem ใน ItemRepository
+	r.GET("/items/:id", itemRepo.GetItem)
+
+	// api /items/:id จะเป็นการเรียกใช้งานฟังก์ชัน UpdateItem ใน ItemRepository
+	r.PUT("/items/:id", itemRepo.UpdateItem)
+
+	// api /items/:id จะเป็นการเรียกใช้งานฟังก์ชัน DeleteItem ใน ItemRepository
+	r.DELETE("/items/:id", itemRepo.DeleteItem)
+
+	r.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{"message": "Not found"})
+	})
+
+	// Run the server
+	if err := r.Run(":5000"); err != nil {
+		log.Fatalf("Server is not running: %v", err)
+	}
+}
